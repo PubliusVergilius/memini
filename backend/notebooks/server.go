3af4b3115ff377dbf9a7be0b322f49d286c69133
M. Vinícius <vinteesetedemaiodedoismil@gmail.com>
2025-05-09 12:50:56 -0400

    add route for get note by id

--- backend/notebooks/server.go
+++ backend/notebooks/server.go
@@ -3,13 +3,25 @@ package notebooks
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-type StubNotebookStore struct {
-	notes map[string]string
+// NoteStore stores notes
+type NoteStore interface {
+	GetNoteById(id string) string
 }
 
-func NotebooksServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Olá")
-	
+type NotebookServer struct {
+	store NoteStore
+}
+
+func (n *NotebookServer) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	noteId := strings.TrimPrefix(r.URL.Path, "/notes/")
+	note := n.store.GetNoteById(noteId)
+
+	if note == "" {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, "error test")
 }
--- backend/notebooks/server_test.go
+++ backend/notebooks/server_test.go
@@ -1,27 +1,84 @@
 package notebooks
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 )
 
+type StubNotebookStore struct {
+	notes map[string]string
+}
 
+func (s *StubNotebookStore) GetNoteById(id string) string {
+	note := s.notes[id]
+	return note
+}
 
 func TestGETNotebooks (t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-	
-	NotebooksServer(response, request)
-	
-	t.Run("returns notebooks", func(t *testing.T){
-		got := response.Body.String()
-		want := "Olá"
+	store := StubNotebookStore{
+		map[string]string{
+			"1" : "teste 1",
+			"2" : "teste 2",
+		},
+	}
+	server := &NotebookServer{&store}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-	
+	tests := []struct {
+		testName           string
+		noteId             string
+		expectedHTTPStatus int
+		expectedNote       string
+	}{
+		{
+			testName: "Returns first note",
+			noteId: "1",
+			expectedHTTPStatus: http.StatusOK,
+			expectedNote: "teste 1",
+		},
+		{
+			testName: "Returns second note",
+			noteId: "2",
+			expectedHTTPStatus: http.StatusOK,
+			expectedNote: "teste 2",
+		},
+		{
+			testName: "Returns 404 on misssing note",
+			noteId: "3",
+			expectedHTTPStatus: http.StatusNotFound,
+			expectedNote: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			request := newGetNoteRequest(tt.noteId)
+			response := httptest.NewRecorder()
+
+			server.ServerHTTP(response, request)
+
+			assertStatus(t, response.Code, tt.expectedHTTPStatus)
+		})
+	}
 }
+
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
+
+func newGetNoteRequest(id string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/notes/%s", id), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string){
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
+
